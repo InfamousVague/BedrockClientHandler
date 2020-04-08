361e38802e148bb985e231517f6382f5cfb6e7eb
RetroPronghorn <retropronghorn@gmail.com>
2020-04-08 13:24:13 -0400

    feat(cleanUser): Cleanup user prefix after LuckPerms group is added.

--- src/main/java/com/github/retropronghorn/bedrockhandler/BedrockHandler.java
+++ src/main/java/com/github/retropronghorn/bedrockhandler/BedrockHandler.java
@@ -32,31 +32,46 @@ public final class BedrockHandler extends JavaPlugin implements Listener {
         String displayName = player.getDisplayName();
 
         if (displayName.contains(bedrockPrefix)) {
-            player.sendTitle("Alpha Warning!", "Bedrock support is in alpha.");
             // LuckPerms Handler
+            notifyUser(player);
             setLuckPermsGroup(player);
+            cleanUser(player);
         }
         // TODO Add Tebex.io hook to remove username prefix and apply packages correctly
 
     }
 
+    // Remove prefixes, and other cleanup tasks
+    private void cleanUser(Player player) {
+        // Remove prefix now that we have a LuckPerms group
+        player.setDisplayName(player.getDisplayName().substring(bedrockPrefix.length()));
+    }
+
+    // Let the player know they are connecting with a partially supported version.
+    private void notifyUser(Player player) {
+        player.sendTitle(
+                "Alpha Warning!",
+                "Heads up, " + player.getDisplayName() + " bedrock is not complete.",
+                10,
+                70,
+                10);
+    }
+
     // Set users LuckPerms group
     private void setLuckPermsGroup(Player player) {
-        System.out.println("Setting up luckperms on user: " + player.getUniqueId());
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
 
         if (provider != null) {
-            System.out.println("Luckperms is not null");
             LuckPerms api = provider.getProvider();
-
             User user = api.getUserManager().getUser(player.getUniqueId());
+
             assert user != null;
+
             String currentLuckPermsGroup = user.getPrimaryGroup();
-            System.out.println("Luck perms group " + currentLuckPermsGroup);
             System.out.println(!currentLuckPermsGroup.equals(bedrockGroupName));
+
             if (!currentLuckPermsGroup.equals(bedrockGroupName)) {
-                System.out.println("Doesn't have group " + bedrockGroupName);
-                user.setPrimaryGroup(bedrockGroupName);
+                System.out.println(user.setPrimaryGroup("group." + bedrockGroupName));
                 api.getUserManager().saveUser(user);
                 System.out.println(user.getPrimaryGroup());
             }
